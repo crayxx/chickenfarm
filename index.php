44408fde683994ab7cd8673d5f41b952a5df3434
Michael Schwarz <michael.schwarz@tum.de>
2017-01-21 15:57:37 +0100

    initialcommit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+    <link href="./css/normalize.css" rel="stylesheet" type="text/css"/>
+    <link href="./css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
+<header>
+    <div class="container">
+        <h1>Cover</h1>
+        <nav>
+            <a href="#">home</a>
+            <a href="#">settings</a>
+        </nav>
+    </div>
+</header>
+
+<main>
+    <h1>It's so easy!</h1>
+    <button>Toggle Alarmclock</button>
+    <br/>
+    <button>Set Alarm in x Seconds</button>
+</main>
+
+<footer>
+    <div class="container">
+        Hier kommen dann die tollen Sachen hin,<br/>
+        die so im footer stehen!
+    </div>
+</footer>
+</body>
+</html>
